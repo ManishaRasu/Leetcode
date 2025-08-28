ba61f6042ad7bc4afc8e9cf98a5dc8039f386462
ManishaRasu <manisharasu05@gmail.com>
2025-08-28 21:28:37 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (95.49%) - LeetSync

---
+++ 1221-element-appearing-more-than-25-in-sorted-array/element-appearing-more-than-25-in-sorted-array.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findSpecialInteger(int[] arr) {
+        int n = arr.length;
+        int q = n / 4;
+
+        for (int i = 0; i < n - q; i++) {
+            if (arr[i] == arr[i + q]) {
+                return arr[i];
+            }
+        }
+
+        return -1;
+    }
+}
